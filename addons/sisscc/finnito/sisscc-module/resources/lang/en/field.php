dad3d4ca270377a3bce2ba3dcd68000076c9e09d
Finn Le Sueur <finn@lesueur.nz>
2021-05-30 15:50:07 +1200

    Fix date placeholder

--- addons/sisscc/finnito/sisscc-module/resources/lang/en/field.php
+++ addons/sisscc/finnito/sisscc-module/resources/lang/en/field.php
@@ -5,7 +5,7 @@ return [
     "name.placeholder" => "SISSCC 2021",
     "slug.name" => "URL Slug",
     "slug.placeholder" => "2021",
-    "date.placeholder" => "28-05-2021",
+    "date.placeholder" => "2021-09-02",
     "date.name" => "Event Date",
     "public.name" => "Event Public",
     "links.name" => "Links",
